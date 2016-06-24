b4baa4e97cb77cd2409befe3ec44b5a0fca2d68b
chabi26 <e.vedogbeton@gmail.com>
2016-06-24 17:04:48 +0200

    Revision hibernate

--- src/test/InitDB.java
+++ src/test/InitDB.java
@@ -1,5 +1,29 @@
 package test;
 
-public class InitDB {
+import java.util.GregorianCalendar;
 
+import jpa.Event;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+public class InitDB {
+    public static void main(String[] args){
+    	//Récupération d'une session hibernate
+    	Session session = HibernateUtils.getSession();
+    	//Début de la transaction
+    	Transaction transaction = session.beginTransaction();
+    	//Création d'un objet Event 	
+    	Event e = new Event();
+    	e.setTitle("Titre de l'event 1");
+    	e.setDescription("Pour le lancement du nouveau single");
+    	e.setBeginDate(new GregorianCalendar());
+    	e.setAllDay(false);
+    	//Enregistrement de l'event
+    	session.save(e);
+    	//Fin de la transaction
+    	transaction.commit();
+    	//fermerture de la session hibernate 
+    	
+    }
 }

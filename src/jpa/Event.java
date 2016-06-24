cc1cd22c110e7ebfb7551b7710b35ba9f7beddde
chabi26 <e.vedogbeton@gmail.com>
2016-06-24 16:49:34 +0200

    Revision hibernate

---
+++ src/jpa/Event.java
@@ -0,0 +1,105 @@
+package jpa;
+
+import java.util.Calendar;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name = "events_016")
+public class Event {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    @Column(name = "events_id")
+	private Integer id;
+    @Column(name = "events_title", nullable =false)
+	private String title;
+    @Column(name = "events_description")
+	private String description;
+    @Column(name = "events_beginDate")
+    @Temporal(TemporalType.DATE)
+	private Calendar beginDate;
+    @Column(name = "events_allDay")
+	private boolean allDay;
+    
+	/**
+	 * @return the id
+	 */
+	public Integer getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the title
+	 */
+	public String getTitle() {
+		return title;
+	}
+
+	/**
+	 * @param title the title to set
+	 */
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	/**
+	 * @return the description
+	 */
+	public String getDescription() {
+		return description;
+	}
+
+	/**
+	 * @param description the description to set
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	/**
+	 * @return the beginDate
+	 */
+	public Calendar getBeginDate() {
+		return beginDate;
+	}
+
+	/**
+	 * @param beginDate the beginDate to set
+	 */
+	public void setBeginDate(Calendar beginDate) {
+		this.beginDate = beginDate;
+	}
+
+	/**
+	 * @return the allDay
+	 */
+	public boolean isAllDay() {
+		return allDay;
+	}
+
+	/**
+	 * @param allDay the allDay to set
+	 */
+	public void setAllDay(boolean allDay) {
+		this.allDay = allDay;
+	}
+	
+	public Event()
+	{
+		
+	}
+}
---
+++ src/test/HibernateUtils.java
@@ -0,0 +1,25 @@
+package test;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.AnnotationConfiguration;
+
+public class HibernateUtils {
+
+	private static final SessionFactory sessionFactory;
+	
+	// Crée une unique instance de la SessionFactory à partir de hibernate.cfg.xml
+	static{
+		try{
+		sessionFactory = (SessionFactory) new AnnotationConfiguration().configure();
+		}catch(HibernateException ex){
+			throw new RuntimeException("Problème de configuration"+ex.getMessage(), ex);
+		}
+	}
+	
+	public static Session getSession() throws HibernateException{
+		return sessionFactory.openSession();
+	}
+	
+}
